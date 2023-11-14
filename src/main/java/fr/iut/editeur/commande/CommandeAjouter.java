13d8f0b497bec957bc6d77a0933738077107ad71
bramandl <lilian.bramand@etu.umontpellier.fr>
2023-11-14 14:30:29 +0100

    javadoc

--- src/main/java/fr/iut/editeur/commande/CommandeAjouter.java
+++ src/main/java/fr/iut/editeur/commande/CommandeAjouter.java
@@ -6,10 +6,19 @@ public class CommandeAjouter extends CommandeDocument {
 
     private static final String DescriptionCommande = "ajouter;texte\n  Ajoute le texte à la fin du document\n";
 
+    /**
+     * Constructeur de la classe CommandeAjouter.
+     *
+     * @param document   le document sur lequel s'applique la commande.
+     * @param parameters les paramètres de la commande.
+     */
     public CommandeAjouter(Document document, String[] parameters) {
         super(document, parameters, DescriptionCommande);
     }
 
+    /**
+     * Exécute la commande d'ajout de texte au document.
+     */
     @Override
     public void executer() {
         if(parameters.length < 2) {
@@ -21,6 +30,11 @@ public class CommandeAjouter extends CommandeDocument {
         super.executer();
     }
 
+    /**
+     * Renvoie la description de la commande.
+     *
+     * @return la description de la commande.
+     */
     public String getDescriptionCommande() {
         return DescriptionCommande;
     }
--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -4,33 +4,69 @@ public class Document {
 
     private String texte;
 
+    /**
+     * Constructeur de la classe Document.
+     */
     public Document() {
         this.texte = "";
     }
-	
+
+    /**
+     * Renvoie le texte du document.
+     *
+     * @return le texte du document.
+     */
     public String getTexte() {
         return texte;
     }
 
+    /**
+     * Modifie le texte du document.
+     *
+     * @param texte le nouveau texte du document.
+     */
     public void setTexte(String texte) {
         this.texte = texte;
     }
 
+    /**
+     * Ajoute du texte à la fin du document.
+     *
+     * @param texte le texte à ajouter.
+     */
     public void ajouter(String texte) {
         this.texte += texte;
     }
 
+    /**
+     * Renvoie une représentation sous forme de chaîne de caractères du document.
+     *
+     * @return la chaîne de caractères représentant le document.
+     */
     @Override
     public String toString() {
         return this.texte;
     }
 
+    /**
+     * Remplace une partie du texte par une autre chaîne de caractères.
+     *
+     * @param start        l'indice de début de la partie à remplacer.
+     * @param end          l'indice de fin de la partie à remplacer.
+     * @param remplacement la nouvelle chaîne de caractères de remplacement.
+     */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
         texte = leftPart + remplacement + rightPart;
     }
 
+    /**
+     * Met en majuscules une partie du texte.
+     *
+     * @param start l'indice de début de la partie à mettre en majuscules.
+     * @param end   l'indice de fin de la partie à mettre en majuscules.
+     */
     public void majuscules(int start, int end) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
@@ -38,16 +74,31 @@ public class Document {
         texte = leftPart + toUpper + rightPart;
     }
 
+    /**
+     * Efface une partie du texte.
+     *
+     * @param start l'indice de début de la partie à effacer.
+     * @param end   l'indice de fin de la partie à effacer.
+     */
     public void effacer(int start, int end) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
         texte = leftPart + rightPart;
     }
 
+    /**
+     * Efface tout le texte du document.
+     */
     public void clear() {
         texte = "";
     }
 
+    /**
+     * Insère une chaîne de caractères à une position donnée du texte.
+     *
+     * @param start     l'indice de la position d'insertion.
+     * @param insertion la chaîne de caractères à insérer.
+     */
     public void inserer(int start, String insertion) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(start);
@@ -55,6 +106,12 @@ public class Document {
 
     }
 
+    /**
+     * Met en minuscules une partie du texte.
+     *
+     * @param start l'indice de début de la partie à mettre en minuscules.
+     * @param end   l'indice de fin de la partie à mettre en minuscules.
+     */
     public void minuscules(int start, int end) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
